787f02f4c720e3e8391b4537c34a24a5dc119490
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-10-02 10:23:19 -0300

    🚧 Tests

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
  * @copyright Copyright (c) 2023
  *
  */
+#include "Database.hpp"
 #include "GameController.hpp" ///< Include the GameController class header
 #include <stdlib.h> ///< Include the standard library for EXIT_SUCCESS
 
@@ -24,6 +25,67 @@ int main(int argc, char* argv[]) {
 
    // Initialize the GameController with command-line arguments
    app.initialize(argc, argv);
+
+   Bar bar1_1 {"Pedro", "", "Masculino", 10};
+   Bar bar1_2 {"Moisés", "", "Masculino", 20};
+   Bar bar1_3 {"Ana", "", "Feminino", 15};
+   Bar bar1_4 {"Renata", "", "Feminino", 40};
+
+   BarChart bar_chart1 {"01-01-1999"};
+   bar_chart1.addBar(&bar1_1);
+   bar_chart1.addBar(&bar1_2);
+   bar_chart1.addBar(&bar1_3);
+   bar_chart1.addBar(&bar1_4);
+
+   Bar bar2_1 {"Pedro", "", "Masculino", 50};
+   Bar bar2_2 {"Moisés", "", "Masculino", 20};
+   Bar bar2_3 {"Ana", "", "Feminino", 30};
+   Bar bar2_4 {"Renata", "", "Feminino", 45};
+
+   BarChart bar_chart2 {"01-01-2000"};
+   bar_chart2.addBar(&bar2_1);
+   bar_chart2.addBar(&bar2_2);
+   bar_chart2.addBar(&bar2_3);
+   bar_chart2.addBar(&bar2_4);
+
+   Bar bar3_1 {"Pedro", "", "Masculino", 70};
+   Bar bar3_2 {"Moisés", "", "Masculino", 110};
+   Bar bar3_3 {"Ana", "", "Feminino", 220};
+   Bar bar3_4 {"Renata", "", "Feminino", 10};
+   Bar bar3_5 {"Ahtgk", "Klan autres", "Alien", 1};
+
+   BarChart bar_chart3 {"01-01-2001"};
+   bar_chart3.addBar(&bar3_1);
+   bar_chart3.addBar(&bar3_2);
+   bar_chart3.addBar(&bar3_3);
+   bar_chart3.addBar(&bar3_4);
+   bar_chart3.addBar(&bar3_5);
+
+   Bar bar4_1 {"Pedro", "", "Masculino", 10};
+   Bar bar4_2 {"Moisés", "", "Masculino", 20};
+   Bar bar4_3 {"Ana", "", "Feminino", 500};
+   Bar bar4_4 {"Renata", "", "Feminino", 23};
+   Bar bar4_5 {"Ahtgk", "Klan autres", "Alien", 4};
+   Bar bar4_6 {"Jdoii", "Klan Uhinh", "Alien", 25};
+
+   BarChart bar_chart4 {"01-01-2002"};
+   bar_chart4.addBar(&bar4_1);
+   bar_chart4.addBar(&bar4_2);
+   bar_chart4.addBar(&bar4_3);
+   bar_chart4.addBar(&bar4_4);
+   bar_chart4.addBar(&bar4_5);
+   bar_chart4.addBar(&bar4_6);
+
+   Database database { "Nomes de crianças no século XXI", "Milhões", "Source: NASA"};
+   database.addCategory("Masculino", fos::foreground::blue);
+   database.addCategory("Feminino", fos::foreground::red);
+   database.addBarChart(&bar_chart1);
+   database.addBarChart(&bar_chart2);
+   database.addCategory("Alien", fos::foreground::green);
+   database.addBarChart(&bar_chart3);
+   database.addBarChart(&bar_chart4);
+
+   database.draw(1, 40, 6, 5);
 
    // Main game loop.
    while (!app.gameOver()) {
