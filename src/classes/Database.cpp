add9bfb853902b7000250d77ad460911e598965f
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-10-06 17:11:35 -0300

    ✨ Smalls updated

--- src/classes/Database.cpp
+++ src/classes/Database.cpp
@@ -1,9 +1,12 @@
 /**
  * @file Database.cpp
  * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
- * @brief
+ * @brief Implementation of the Database class.
  * @version 1.0
  * @date 2023-10-02
+ *
+ * This file contains the implementation of the Database class, which is
+ * responsible for storing and managing bar chart data.
  *
  * @copyright Copyright (c) 2023
  *
@@ -12,48 +15,65 @@
 #include "Database.hpp"
 
 Database::~Database() {
-   bar_charts.clear();
+   // Destructor implementation
 }
 
+// Function to get the title of the database
 string Database::getTitle() const {
    return title;
 }
+
+// Function to get the scale of the database
 string Database::getScale() const {
    return scale;
 }
+
+// Function to get the source of the database
 string Database::getSource() const {
    return source;
 }
+
+// Function to get the categories stored in the database
 map<string, short> Database::getCategories() const {
    return categories;
 }
+
+// Function to get the bar charts stored in the database
 vector<shared_ptr<BarChart>> Database::getBarCharts() const {
    return bar_charts;
 }
 
+// Function to set the title of the database
 void Database::setTitle(string title_) {
    title = title_;
 }
+
+// Function to set the scale of the database
 void Database::setScale(string scale_) {
    scale = scale_;
 }
+
+// Function to set the source of the database
 void Database::setSource(string source_) {
    source = source_;
 }
+
+// Function to add a category to the database
 bool Database::addCategory(string text, short color) {
    if (categories.find(text) == categories.end()) {
       categories[text] = color;
-         if (categories.size() > NUMBER_OF_COLORS) {
-      for (auto& category : categories) {
-         category.second = fos::foreground::green;
+      if (categories.size() > NUMBER_OF_COLORS) {
+         for (auto& category : categories) {
+            category.second = fos::foreground::green;
+         }
       }
-   }
-   return true;
+      return true;
    }
 
    return false;
 }
 
+// Function to add a category to the database
 void Database::addBarChart(BarChart const* bar_chart) {
    if (bar_chart) {
       shared_ptr<BarChart> ptr { make_shared<BarChart>(*bar_chart) };
@@ -61,6 +81,7 @@ void Database::addBarChart(BarChart const* bar_chart) {
    }
 }
 
+// Function to add a category to the database
 void Database::draw(short fps, short bar_size, short _view_bars, short _ticks,
   short _terminal_size) const {
    for (shared_ptr<BarChart> bar_chart : bar_charts) {
@@ -82,10 +103,10 @@ void Database::draw(short fps, short bar_size, short _view_bars, short _ticks,
       cout << setStyle(source, -1, -1, bold) << "\n";
 
       if (categories.size() <= NUMBER_OF_COLORS) {
-      for (auto category : categories) {
-         cout << setStyle("\u2588", category.second, -1, bold) << ": ";
-         cout << setStyle(category.first, category.second, -1, bold) << " ";
-      }
+         for (auto category : categories) {
+            cout << setStyle("\u2588", category.second, -1, bold) << ": ";
+            cout << setStyle(category.first, category.second, -1, bold) << " ";
+         }
       }
 
       cout << "\n\n";
--- src/classes/Database.hpp
+++ src/classes/Database.hpp
@@ -1,71 +1,167 @@
 /**
  * @file Database.hpp
  * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
- * @brief 
+ * @brief Represents a database for storing and managing bar chart data.
  * @version 1.0
  * @date 2023-10-02
- * 
+ *
+ * The Database class provides functionality for storing information about bar
+ * charts, including their titles, scales, sources, categories, and actual bar
+ * chart data.
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #ifndef DATABASE_HPP_
 #define DATABASE_HPP_
 
 #include "BarChart.hpp"
+
 #include <vector> /// vector
 using std::vector;
+
 #include <string> /// string
 using std::string;
+
 #include <memory> /// shared_ptr, make_shared
 using std::make_shared;
 using std::shared_ptr;
+
 #include <map> /// map
 using std::map;
+
 #include <iostream> /// iostream
 using std::cout;
 
 #include <chrono>
 #include <thread>
 
-#include "fstring.hpp" /// setStyle, blue, yellow, bold
-using fos::setStyle;
-using fos::columnWrap;
+#include "fstring.hpp" /// alignment, columnWrap, setStyle, center, blue, yellow, bold
 using fos::alignment;
+using fos::columnWrap;
+using fos::setStyle;
+using fos::align::center;
 using fos::foreground::blue;
 using fos::foreground::yellow;
-using fos::align::center;
 using fos::style::bold;
 
-constexpr short NUMBER_OF_COLORS { 14 };
+///< Definition of system constants >//
+constexpr short NUMBER_OF_COLORS { 14 }; ///< Number of colors >//
 
+/**
+ * @class Database
+ * @brief Represents a database for storing and managing bar chart data
+ *
+ * The Database class provides functionality for storing information about bar
+ * charts, including their titles, scales, sources, categories, and actual bar
+ * chart data.
+ */
 class Database {
    public:
+   /**
+    * @brief Default constructor for the Database class
+    */
+   Database() { }
 
-   Database(){}
-   Database(string title_, string scale_, string source_) : title(title_), scale(scale_), source(source_) {}
+   /**
+    * @brief Constructor for the Database class
+    * @param title_ Title of the database
+    * @param scale_ Scale of the database
+    * @param source_ Source of the database
+    */
+   Database(string title_, string scale_, string source_)
+       : title(title_), scale(scale_), source(source_) { }
+
+   /**
+    * @brief Destructor for the Database class
+    */
    ~Database();
 
+   /**
+    * @brief Get the title of the database
+    * @return The title of the database
+    */
    string getTitle() const;
+
+   /**
+    * @brief Get the scale of the database
+    * @return The scale of the database
+    */
    string getScale() const;
+
+   /**
+    * @brief Get the source of the database
+    * @return The source of the database
+    */
    string getSource() const;
+
+   /**
+    * @brief Get the categories stored in the database
+    * @return A map of category names to colors
+    */
    map<string, short> getCategories() const;
+
+   /**
+    * @brief Get the bar charts stored in the database
+    * @return A vector of shared pointers to BarChart objects
+    */
    vector<shared_ptr<BarChart>> getBarCharts() const;
 
+   /**
+    * @brief Set the title of the database
+    * @param title_ The new title for the database
+    */
    void setTitle(string title_);
+
+   /**
+    * @brief Set the scale of the database
+    * @param scale_ The new scale for the database
+    */
    void setScale(string scale_);
+
+   /**
+    * @brief Set the source of the database
+    * @param source_ The new source for the database
+    */
    void setSource(string source_);
+
+   /**
+    * @brief Add a category to the database
+    * @param text The name of the category
+    * @param color The color associated with the category
+    * @return True if the category was added successfully, false otherwise
+    */
    bool addCategory(string text, short color);
+
+   /**
+    * @brief Add a bar chart to the database
+    * @param bar_chart A pointer to a BarChart object to add
+    */
    void addBarChart(BarChart const* bar_chart);
 
-   void draw(short fps, short bar_size, short _view_bars = 0, short _ticks = 0, short _terminal_size = 0) const;
+   /**
+    * @brief Draw the database's content with customizable parameters
+    * @param fps Frames per second for the drawing
+    * @param bar_size Size of the bars in the chart
+    * @param _view_bars Number of bars to display (0 to show all)
+    * @param _ticks Number of ticks to display (0 for none)
+    * @param _terminal_size Terminal size for proper formatting (0 for default)
+    */
+   void draw(short fps, short bar_size, short _view_bars = 0, short _ticks = 0,
+     short _terminal_size = 0) const;
 
    private:
-   string title;
-   string scale;
-   string source;
-   map<string, short> categories;
-   vector<shared_ptr<BarChart>> bar_charts;
+   string title; ///< Title of the database
+
+   string scale; ///< Scale of the database
+
+   string source; ///< Source of the database
+
+   map<string, short> categories; ///< Map of category names to colors
+
+   vector<shared_ptr<BarChart>>
+     bar_charts; ///< Vector of shared pointers to BarChart objects
 };
 
 #endif /// DATABASE_HPP_
